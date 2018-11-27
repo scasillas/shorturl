18725e46ae0d03d2830f3b998ed381679fb2d381
Glitch (thirsty-macaw) <none>
2018-11-27 12:21:58 +0000

    🍜⚡️ Checkpoint ./index.js:2177980/232

---
+++ index.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var express = require('express');
+var googl = require('goo.gl');
+
+var app = express();
+
+if(!process.env.DISABLE_XORIGIN){
+app.use(function(req,res, next){
+var allowed
+}
+}
